c6aad94c79f045b2ce76b4b3cb880c420c8aafd8
TranVanThai <vanthaia7@gmail.com>
2025-04-11 17:01:05 +0700

    more history add

--- src/components/HistoryPageSection.vue
+++ src/components/HistoryPageSection.vue
@@ -37,7 +37,7 @@
         </div>
     </section>
 
-    <section class="vietnam-history-detail py-5">
+    <section class="vietnam-history-menu py-5">
         <div class="container">
             <h2 class="text-dark text-center fw-bold">Lịch sử Việt Nam cụ thể qua các thời kỳ</h2>
             <div class="row align-items-center">
@@ -47,7 +47,9 @@
                     <div class="history-item">
                         <h5>1. Thời tiền sử</h5>
                         <ul class="list-unstyled">
-                            <li class="hover-item">1.1 Thời đồ đá</li>
+                            <li class="hover-item">
+                                <a class="text-decoration-none hover-item"
+                                 href="#thoiDoDa">1.1 Thời đồ đá</a></li>
                             <li class="hover-item">1.2 Thời đồ đồng đá</li>
                             <li class="hover-item">1.3 Thời đồ đồng</li>
                             <li class="hover-item">1.4 Thời đồ sắt</li>
@@ -57,14 +59,20 @@
                     <div class="history-item">
                         <h5>2. Thời kỳ cổ đại</h5>
                         <ul class="list-unstyled">
-                            <li class="hover-item">2.1 Hồng Bàng và Văn Lang</li>
+                            <li class="hover-item">
+                                <a class="text-decoration-none hover-item"
+                                 href="#thoiHBVL">2.1 Hồng Bàng và Văn Lang</a>
+                            </li>
                             <li class="hover-item">2.2 Nhà Thục</li>
                         </ul>
                     </div>
                     <div class="history-item">
                         <h5>3. Thời Bắc thuộc (180 TCN – 938)</h5>
                         <ul class="list-unstyled">
-                            <li class="hover-item">3.1 Nhà Triệu (180 TCN – 111 TCN)</li>
+                            <li class="hover-item">
+                                <a href="#thoiNTr"  class="text-decoration-none hover-item">
+                                    3.1 Nhà Triệu (180 TCN – 111 TCN)</a>
+                            </li>
                             <li class="hover-item">3.2 Bắc thuộc lần 1 (111 TCN – 40 SCN)</li>
                             <li class="hover-item">3.3 Hai Bà Trưng (40 – 43)</li>
                             <li class="hover-item">3.4 Bắc thuộc lần 2 (43 – 544)</li>
@@ -90,17 +98,129 @@
                             <li class="hover-item">5.2 Thời kỳ cộng hòa (1945 – nay)</li>
                         </ul>
                     </div>
-
-
-
-
                 </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="vietnam-history-detail py-5 bg-secondary">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- các chương -->
+                <div class="col-lg-12 col-sm-12">
+                    <h2 class="text-white">1. Thời tiền sử</h2>
+                    <h3 id="thoiDoDa" class="text-dark bg-light" >1.2 Thời đồ đá</h3>
+                    <p class="text-white">Ngay từ thời đại đồ đá cũ cách
+                        đây hàng trăm nghìn năm, vùng đất Việt Nam đã có người sinh sống. Các nhà khảo cổ học đã tìm thấy những dấu tích về sự có mặt của con người tiền sử tại các hang động, núi đá như hang Thẩm Hoi, Thẩm Khuyên ở Lạng Sơn, núi Đọ ở Thanh Hóa. Họ sống bằng săn bắt và hái lượm trong môi trường tự nhiên hoang sơ.
+                        Sang thời kỳ đồ đá mới cách đây 15.000 – 18.000 năm, 
+                        người Việt cổ đã chuyển sang sống định cư, biết trồng 
+                        lúa nước và chế tác công cụ lao động từ đá. Đây chính là nền 
+                        tảng cho sự ra đời của nền văn minh nông nghiệp lúa nước ở Việt Nam.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="thoiDoDa" class="text-dark bg-light">1.2 Thời đồ đồng đá</h3>
+                    <p class="text-white">Ngay từ thời đại đồ đá cũ cách
+                        Khoảng 4000 – 3500 năm trước Công nguyên, nền văn hóa Phùng Nguyên ra đời,
+                         đánh dấu sự khởi đầu của thời đại đồ đồng đá ở Việt Nam. 
+                        Thời kỳ này, người Việt đã biết khai thác và đúc những đồ đồng thô sơ từ các mỏ đồng tự nhiên. 
+                        Đây là bước tiến quan trọng trong lịch sử kỹ thuật của người Việt cổ.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="thoiDoDa" class="text-dark bg-light">1.3. Thời đại đồ đồng</h3>
+                    <p class="text-white">Đến khoảng 3000 năm trước Công nguyên, nền văn hóa Đồng Đậu xuất hiện.
+                         Người Việt đã đúc được những đồ đồng tinh xảo và đẹp mắt hơn. Họ cũng đã biết kỹ thuật đánh bóng và trang trí hoa văn trên đồ đồng. 
+                        Đồ đồng Đồng Đậu có giá trị lịch sử và nghệ thuật cao.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="thoiDoDa" class="text-dark bg-light">1.4. Thời đại đồ sắt</h3>
+                    <p class="text-white">Vào khoảng 1200 TCN, nền văn hóa Đông Sơn ra đời ở khu vực sông Mã và đồng bằng sông Hồng,
+                         đánh dấu thời kỳ hoàng kim của nghệ thuật đúc đồng Việt Nam. Những chiếc trống đồng 
+                         Đông Sơn tinh xảo với hoa văn được chạm khắc công phu đã trở thành biểu tượng của văn hóa Việt cổ.
+                          Các nhà khảo cổ cũng tìm thấy nhiều hiện vật quý giá khác như vũ khí,
+                         dụng cụ, đồ trang sức làm bằng đồng thể hiện trình độ kỹ thuật cao của thời kỳ này.</p>
+                </div>
+
+                <!-- CHƯƠNG 2 -->
+                <div class="col-lg-12 col-sm-12">
+                    <h2 class="text-white">2. Thời kỳ cổ đại</h2>
+                    <h3 id="thoiHBVL" class="text-dark bg-light">2.1. Hồng Bàng và Văn Lang</h3>
+                    <p class="text-white">Theo truyền thuyết, vào khoảng năm 2879 TCN, nhà nước Xích Quỷ của người Việt ra đời ở khu vực phía Nam sông Dương Tử.
+                         Tuy nhiên, sự tồn tại của nhà nước này chưa được khảo cổ học xác nhận.
+                        Đến thế kỷ thứ VII TCN, vương quốc Văn Lang của người Việt được thành lập ở khu vực trung du và đồng bằng Bắc Bộ.
+                        Nhà nước Văn Lang có bộ máy chính quyền tương đối hoàn chỉnh, chia ra các đơn vị hành chính cấp bộ, làng. 
+                        Người dân Văn Lang sống bằng nông nghiệp lúa nước.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">2.2. Nhà Thục</h3>
+                    <p class="text-white">Đến thế kỷ thứ III TCN, Thục Phán – thủ Lĩnh bộ tộc Âu Việt,
+                         đã đánh bại vua Hùng thứ 18, thống nhất các bộ tộc Lạc Việt và Âu Việt. 
+                        Thục Phán tự xưng là An Dương Vương, lập ra nhà nước Âu Lạc đóng đô tại Cổ Loa (nay là Hà Nội).</p>
+                </div>
+                
+                <!-- CHƯƠNG 3 -->
+                <div class="col-lg-12 col-sm-12">
+                    <h2 class="text-white">3. Thời Bắc thuộc (180 TCN – 938)</h2>
+                    <h3 id="thoiNTr" class="text-dark bg-light">3.1. Nhà Triệu (180 TCN – 111 TCN)</h3>
+                    <p class="text-white">Trong lúc nhà Tần gặp khó khăn sau cái chết của Tần Thủy Hoàng vào năm 210 TCN, 
+                        Triệu Đà mở rộng lãnh thổ. Năm 179 TCN, Triệu Đà thôn tính nước Âu Lạc của An Dương Vương, lập nên nước Nam Việt độc lập, kinh đô tại Phiên Ngung.
+                         Nhà Triệu cai trị trong 68 năm, từ 179 TCN đến 111 TCN.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">3.2. Bắc thuộc lần 1 (111 TCN – 40 SCN)</h3>
+                    <p class="text-white">Năm 111 TCN, sau khi thống nhất Trung Hoa, Hán Vũ Đế cho quân xâm chiếm Nam Việt và sáp nhập vào lãnh thổ nhà Hán. 
+                        Chính sách đồng hóa người Việt bằng văn hóa Hán bắt đầu được thực hiện khiến người Việt phải sống dưới ách đô hộ nặng nề của phương Bắc.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">3.3. Hai Bà Trưng (40 – 43)</h3>
+                    <p class="text-white">Năm 40, Hai Bà Trưng ở quận Giao Chỉ đã dấy binh khởi nghĩa và giành được độc lập.
+                         Trưng Trắc tự xưng là Trưng Nữ Vương. Tuy nhiên, chỉ sau 3 năm, nhà Hán cử Mã Viện sang đàn áp và do tổ chức quân đội chưa hoàn thiện nên không thể chống cự lại, 
+                        Hai Bà Trưng đã tự vẫn trên sông Hát để giữ vẹn khí tiết</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">3.4. Bắc thuộc lần 2 (43 – 544)</h3>
+                    <p class="text-white">Sau thất bại của Hai Bà Trưng, các chính quyền phong kiến Trung Hoa kế tiếp như Đông Ngô, 
+                        Tấn, Lưu Tống, Nam Tề, Lương tiếp tục đô hộ Việt Nam. Dưới ách thống trị này,
+                         người Việt vẫn liên tục đứng lên kháng chiến, tuy thất bại nhưng khí phách vẫn không tắt.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">3.5. Nhà Tiền Lý (544 – 602)</h3>
+                    <p class="text-white">Năm 544, Lý Bí khởi nghĩa đánh đuổi quân Lương, lập ra nước Vạn Xuân độc lập, xưng vương là Lý Nam Đế.
+                    Năm 545, Lý Nam Đế bại trận, giao lại binh quyền cho Triệu Quang Phục. Sau khi Lý Nam Đế qua đời, Triệu Quang Phục đã đánh đuổi được quân Lương năm 550, tự xưng là Triệu Việt Vương.
+                    Đến 571, Lý Phật Tử (cháu của Lý Nam Đế) đã cướp ngôi, nhà nước Vạn Xuân độc lập đến năm 602 bị nhà Tùy sang đánh</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">3.6. Bắc thuộc lần 3 (602 – 905)</h3>
+                    <p class="text-white">Năm 602, nhà Tùy đánh bại nước Vạn Xuân và lại thiết lập Bắc thuộc lần 3 cho Việt Nam.
+                         Tiếp nối nhà Tùy, nhà Đường cũng lần lượt đô hộ Việt Nam gần 300 năm
+                Trong giai đoạn này, người Việt đã tổ chức nhiều cuộc khởi nghĩa chống lại sự cai trị của nhà Đường như Lý Tự Tiên, 
+                Đinh Kiến, Mai Hắc Đế, Phùng Hưng và Dương Thanh từ cuối thế kỷ VII tới thế kỷ IX.</p>
+                </div>
+
+                <div class="col-lg-12 col-sm-12">
+                    <h3 id="#" class="text-dark bg-light">3.7. Thời kỳ tự chủ (905 – 939) – Nhà Khúc</h3>
+                    <p class="text-white">Tới cuối thế kỷ IX, sau những biến cố nội bộ và chiến tranh ở Trung Quốc, nhà Đường suy yếu. Tại Việt Nam,
+                         vào năm 905, Khúc Thừa Dụ đã chiếm lấy thủ phủ Đại La, đánh dấu bước ngoặt thời kỳ tự chủ sau gần 1000 năm Bắc thuộc.</p>
+                </div>
+                <!-- CHƯƠNG 4 -->
+
             </div>
         </div>
     </section>
 </template>
 
 <style>
+template{
+    scroll-behavior: smooth;
+}
 .main-red {
     background-color: #D84040;
 }
@@ -113,7 +233,7 @@
     color: rgb(170, 19, 19);
 
 }
-.vietnam-history-detail .history-item {
+.vietnam-history-menu .history-item {
     width: 220px;
     padding: 10px;
     background: #fff;
