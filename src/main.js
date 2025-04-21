42c22f3d9a2da2dfc22a2ea15f65df8596897ecb
TranVanThai <vanthaia7@gmail.com>
2025-04-21 15:10:28 +0700

    add env

--- src/main.js
+++ src/main.js
@@ -5,13 +5,34 @@ import router from './router';
 import { Drawer, Button, message, List, Menu } from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import '../src/static/fontawesome-free-6.7.2-web/fontawesome-free-6.7.2-web/css/all.min.css'
+import { createI18n } from 'vue-i18n';
+import './GlobalCSS/main.scss'
 
+const messages = {
+    en: {
+      message: {
+        hello: 'hello world',
+        goodbye: 'goodbye world',
+        name: 'My name is ...'
+      }
+    },
+    vi: {
+      message: {
+        hello: 'xin chào thế giới',
+        goodbye: 'tạm biệt thế giới',
+        name: 'Tên tôi là ...'
+      }
+    }
+  };
 
-
-
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en', 
+    messages 
+  });
 const app = createApp(App);
 
-
+app.use(i18n);
 app.use(router)
 app.use(Drawer)
 app.use(List)
